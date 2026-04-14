9f12f8173fbda060bae84539207b6644fa58d067
Matias-Ezeq <matiasezequiel.delarosa@estudiantes.unahur.edu.ar>
2026-04-14 19:10:27 -0300

    ejemplo con "base de datos"

--- proyecto/index.js
+++ proyecto/index.js
@@ -2,5 +2,21 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.listen(port, () => {console.log('Aplicación web corriendo')})
+const productos = require('./data/productos.json')
+
+app.get('/usuarios', (req,res) => {
+    const usuarios = [{nombre:'Juan',edad:20},
+                       {nombre:'Paco',edad:20},
+                       {nombre:'Pedro',edad:20}
+                    ]
+    res.json(usuarios)
+})
+
+app.get('/productos', (req,res) => {
+    res.json(productos)
+})
+
+app.listen(port, () => {
+    console.log('Aplicación web corriendo')
+})
 
