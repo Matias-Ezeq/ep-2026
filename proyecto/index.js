2d42106fade89d766123da6978d7e1e7f4cf3c6c
Matias-Ezeq <matiasezequiel.delarosa@estudiantes.unahur.edu.ar>
2026-04-14 20:26:32 -0300

    postman (sale mal)

--- proyecto/index.js
+++ proyecto/index.js
@@ -16,7 +16,38 @@ app.get('/productos', (req,res) => {
     res.json(productos)
 })
 
+app.post('/productos', (req, res) => {
+    const {nombre, precio, stock} = req.body
+    const id = productos.length + 1
+
+    if(!nombre || precio== null || stock == null) {
+        return res.status(400).json({message: "Los campos nombre, precio, y stock son obligatorios"})
+    }
+    const nuevoProducto = {
+        id,
+        nombre,
+        precio,
+        stock
+    }
+    productos.push(nuevoProducto)
+    res.status(200).json({message: "Producto creado con éxito"})
+})
+
+app.put('/productos/:id', (req,res) => {
+    const {nombre, precio, stock} = req.body
+    const { id } = req.params
+    const indice = productos.findIndex( p => p.id == id)
+    if(!nombre || precio== null || stock == null) {
+        return res.status(400).json({message: "Los campos nombre, precio, y stock son obligatorios"})
+    }
+    productos[indice] = nuevoProducto
+    res.status(200).json({message: "Producto actualizado con exito"})
+})
+
+
+
 app.listen(port, () => {
     console.log('Aplicación web corriendo')
 })
+
 
