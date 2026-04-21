51280e00f70fbd7ea82c45705e70a931226a68f0
Matias-Ezeq <matiasezequiel.delarosa@estudiantes.unahur.edu.ar>
2026-04-21 20:53:52 -0300

    clase 21/4

--- codigo-asincronico/index.js
+++ codigo-asincronico/index.js
@@ -17,6 +17,19 @@ app.get('/productos', async (req,res) => {
     
 })
 
+app.get('/productos/:id', async (req,res) => {
+    try {
+        const { id }  = req.params
+        const producto = await Producto.findByPk(id, {
+            attributes: ["nombre","precio","stock"]
+        })
+        res.status(200).json(producto)
+    }
+    catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
 app.post('/productos', async (req,res) => {
     try {
         const { nombre, precio, stock} = req.body
@@ -34,6 +47,25 @@ app.post('/productos', async (req,res) => {
         res.status(500).json({message: "Error al crear el producto"})
     }
 })
+
+app.put('/productos/:idProducto', async (req,res) => {
+    try {
+        const { idProducto } = req.params
+        const { nombre, precio, stock} = req.body
+        const producto = await Producto.update({
+            nombre,
+            precio,
+            stock
+        },
+        {where: {id: idProducto}}
+        )
+        res.status(201).json({message: "el producto fue actualizado con exito."})
+    }
+    catch (error) {
+        res.status(500).json({message: "Error al actualizar el producto"})
+    }
+})
+
 
 app.listen(PORT, async () => {
     await db.sequelize.sync()
